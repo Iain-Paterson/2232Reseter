3d26fedcd09937afc0adb3ad9b3818f84bf85e20
Iain Paterson <iain@Iains-MacBook-Pro.local>
2019-07-10 13:51:25 +0100

    Tidied up main loop.

--- PortInversionTest/main.cpp
+++ PortInversionTest/main.cpp
@@ -16,39 +16,55 @@
 
 //#pragma clang diagnostic ignored "-Wc++17-extensions"
 
+void GetKeyboardTerminate( void );
+
 using namespace std;
+atomic<bool> run { true };
 
 int main(int argc, const char * argv[]) {
-    char  burl[] = { '/','-','\\','|','/','-','\\','|'};
-    atomic<bool> run { true };
+    //char  burl[] = { '/','-','\\','|','/','-','\\','|'};
+
     shared_ptr<thread> t1;
     
     
     cout << "Hello, World!\n";
     {
-        shared_ptr<DeviceReseter> dr = make_shared<DeviceReseter>(10, 100);
+        shared_ptr<DeviceReseter> dr = make_shared<DeviceReseter>(chrono::seconds(10),chrono::milliseconds( 100) );
         shared_ptr<DualCHTest>    dut= make_shared<DualCHTest>();
         auto [r,t] = dr->start();
         t1 = t;
-        int c=0;
-        cout << "quit?\n";
+        //int c=0;
+        cout << "*";
+        thread gkt(GetKeyboardTerminate);
         while(run)
         {
         
-            this_thread::sleep_for( chrono::microseconds(200) );
-            cout << "\b" << burl[c++ % 8];
+            this_thread::sleep_for( chrono::milliseconds(100) );
+            //cout << "\b\b" << burl[c++ % 8];
+            //cout << ".";
             //if ( getchar() == 'q')
             //    run = false;
             
         }
         dr->stop();
-        
+    
         cout << "bye.\n";
+        gkt.join();
     }
-    
-    
-    
     cout << "caio!\n";
     
     return 0;
+}
+
+void GetKeyboardTerminate( void )
+{
+    cout << "GetKeyboardTerminate+++\n";
+    cout << "quit?\n";
+    while(run)
+    {
+        if ( getchar() == 'q')
+            run = false;
+    }
+    cout << "GetKeyboardTerminate---\n";
+    return;
 }
